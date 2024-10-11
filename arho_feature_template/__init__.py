81f6f180dd9abda69100bb62e7747839fed5e6e8
Lauri Kajan <lauri.kajan@gispo.fi>
2024-10-11 11:39:45 +0300

    Prevent unused imports
    Ruff started to autofix there F401 by removing the imports even the
    F401 is defined as unfixable in pyproject.toml. Reimplement the
    debugging logic for now to mitigate this.
    
    This debugging part should be removed completly some time when
    instructions are written how to debug instead.

--- arho_feature_template/__init__.py
+++ arho_feature_template/__init__.py
@@ -1,21 +1,32 @@
+from __future__ import annotations
+
 import os
 from typing import TYPE_CHECKING
-
-from arho_feature_template.qgis_plugin_tools.infrastructure.debugging import (
-    setup_debugpy,  # noqa: F401
-    setup_ptvsd,  # noqa: F401
-    setup_pydevd,  # noqa: F401
-)
 
 if TYPE_CHECKING:
     from qgis.gui import QgisInterface
 
-debugger = os.environ.get("QGIS_PLUGIN_USE_DEBUGGER", "").lower()
-if debugger in {"debugpy", "ptvsd", "pydevd"}:
-    locals()["setup_" + debugger]()
+    from arho_feature_template.plugin import Plugin
+
+debugger = os.environ.get("QGIS_PLUGIN_USE_DEBUGGER")
+if debugger is not None:
+    from arho_feature_template.qgis_plugin_tools.infrastructure.debugging import (
+        setup_debugpy,
+        setup_ptvsd,
+        setup_pydevd,
+    )
+
+    debugger_setups = {
+        "debugpy": setup_debugpy,
+        "ptvsd": setup_ptvsd,
+        "pydevd": setup_pydevd,
+    }
+    debug_setupper = debugger_setups.get(debugger.lower())
+    if debug_setupper is not None:
+        debug_setupper()
 
 
-def classFactory(iface: "QgisInterface"):  # noqa N802
+def classFactory(iface: "QgisInterface") -> Plugin:  # noqa N802
     from arho_feature_template.plugin import Plugin
 
     return Plugin()
