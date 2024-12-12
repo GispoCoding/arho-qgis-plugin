e9499840091fa46051b0f97977a2febf3b8170b7
Niko Aarnio <niko.aarnio@protonmail.com>
2024-12-12 09:40:39 +0200

    (WIP) refactor plan regulations and plan regulations groups

--- arho_feature_template/core/models.py
+++ arho_feature_template/core/models.py
@@ -1,28 +1,91 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
+from enum import Enum
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from qgis.core import QgsGeometry
+    from numbers import Number
+    from qgis.core import QgsGeometry, QgsFeature
+
+
+class ValueType(Enum):
+    DECIMAL = "desimaali"
+    POSITIVE_DECIMAL = "positiivinen desimaali"
+    POSITIVE_INTEGER = "positiivinen kokonaisluku"
+    POSITIVE_INTEGER_RANGE = "positiivinen kokonaisluku arvoväli"
+    VERSIONED_TEXT = "kieliversioitu teksti"
+
+
+@dataclass
+class RegulationConfig:
+    """
+    Describes plan regulation type.
+    
+    Initialized from DB/QGIS layer and extended with data from a config file.
+    """
+    id: str
+    regulation_code: str
+    name: str
+    description: str
+    status: str
+    level: int
+    parent_id: str | None
+    child_regulations: list[RegulationConfig] = field(default_factory=list)
+
+    # Data from config file
+    category_only: bool = False
+    value_type: ValueType | None = None
+    unit: str | None = None
+
+    # NOTE: Perhaps this ("model_from_feature") should be method of PlanTypeLayer class?
+    @classmethod
+    def from_feature(cls, feature: QgsFeature, language: str = "fin") -> RegulationConfig:
+        """
+        Initialize PlanRegulationConfig from QgsFeature.
+
+        Child regulations, value type ,category only and unit need to be set separately.
+        """
+        return cls(
+            id=feature["id"],
+            regulation_code=feature["value"],
+            name=feature["name"][language],
+            description=feature["description"][language],
+            status=feature["status"],
+            level=feature["level"],
+            parent_id=feature["parent_id"],
+        )
+
+    def add_to_dictionary(self, dictionary: dict[str, RegulationConfig]):
+        """Add child regulations to dictionary too."""
+        dictionary[self.regulation_code] = self
+        for regulation in self.child_regulations:
+            regulation.add_to_dictionary(dictionary)
 
 
 @dataclass
 class Regulation:
-    type_code: str
-    value_string: str | None
-    value_int: int | None
-    value_float: float | None
-    unit: str | None
+    config: RegulationConfig  # includes regulation_code and unit among other needed data for saving feature
+    value: str | Number | tuple[int, int] | None = None
+    regulation_number: int | None
+    additional_information: dict[str, str | Number | None]
+    files: list[str] = []
+    theme: str | None = None
+    topic_tag: str | None = None
     id_: int | None = None
+    # value_string: str | None
+    # value_number: Number | None
+    # value_number_pair: tuple[Number, Number] | None
 
 
 @dataclass
 class RegulationGroup:
-    name: str
-    short_name: str | None
     type_code: str
-    regulations: list[Regulation]
+    name: str | None
+    short_name: str | None
+    color_code: str | None
+    letter_code: str | None
+    regulations: list[Regulation] | list[RegulationConfig]
     id_: int | None = None
 
 
--- arho_feature_template/gui/plan_regulation_widget.py
+++ arho_feature_template/gui/plan_regulation_widget.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 from importlib import resources
 from numbers import Number
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING
 
 from qgis.core import QgsApplication
-from qgis.gui import QgsDoubleSpinBox, QgsFileWidget, QgsSpinBox
+from qgis.gui import QgsFileWidget
 from qgis.PyQt import uic
 from qgis.PyQt.QtCore import Qt, pyqtSignal
 from qgis.PyQt.QtWidgets import (
@@ -13,107 +13,92 @@ from qgis.PyQt.QtWidgets import (
     QLabel,
     QLineEdit,
     QMenu,
-    QSizePolicy,
-    QTextEdit,
     QToolButton,
     QWidget,
 )
 
-from arho_feature_template.core.plan_regulation_config import PlanRegulationConfig, ValueType
+from arho_feature_template.core.plan_regulation_config import ValueType
 from arho_feature_template.utils.misc_utils import get_additional_information_name, get_layer_by_name
+from arho_feature_template.core.models import Regulation, RegulationConfig
+from arho_feature_template.gui.plan_regulation_input_widgets import (
+    DecimalInputWidget,
+    IntegerInputWidget,
+    IntegerRangeInputWidget,
+    MultilineTextInputWidget,
+    SinglelineTextInputWidget
+)
+from arho_feature_template.project.layers.plan_layers import PlanRegulationLayer
+
 
 if TYPE_CHECKING:
     from qgis.PyQt.QtWidgets import QPushButton
 
-    from arho_feature_template.core.plan_regulation_group_config import PlanRegulationDefinition
-
 ui_path = resources.files(__package__) / "plan_regulation_widget.ui"
 FormClass, _ = uic.loadUiType(ui_path)
 
-
-# Related layer and field names to save information later on
-LAYER_NAME = "Kaavamääräys"
-TYPE_OF_PLAN_REGULATION_KIND_FIELD = "type_of_plan_regulation_kind"
-NUMERIC_VALUE_FIELD = "numeric_value"
-TYPE_OF_VERBAL_PLAN_REGULATION_FIELD = "type_of_verbal_plan_regulation"
-UNIT_FIELD = "unit"
-TEXT_VALUE_FIELD = "text_value"
-REGULATION_TYPE_ADDITIONAL_INFORMATION_ID = "regulation_type_additional_information_id"
-
 # TO BE REPLACED
 ADDITIONAL_INFORMATION_TYPES_WITH_INPUT = ["kayttotarkoituskohdistus"]
+LANGUAGE = "fin"
 
 
-class PlanRegulationWidget(QWidget, FormClass):  # type: ignore
+class RegulationWidget(QWidget, FormClass):  # type: ignore
     """A widget representation of a plan regulation."""
-
-    language = "fin"
 
     delete_signal = pyqtSignal(QWidget)
 
-    def __init__(self, config: PlanRegulationConfig, parent=None):
+    def __init__(self, regulation_data: Regulation | RegulationConfig, parent=None):
         super().__init__(parent)
         self.setupUi(self)
 
         # TYPES
         self.plan_regulation_name: QLineEdit
         self.form_layout: QFormLayout
-
         self.add_additional_information_btn: QPushButton
         self.add_field_btn: QPushButton
         self.del_btn: QPushButton
         self.expand_hide_btn: QToolButton
-
         self.code_label: QLabel
         self.code: QLineEdit
 
         # INIT
-        self.config = config
-        self.regulation_number_added = False
-        self.expanded = True
+        if isinstance(regulation_data, Regulation):
+            self.config = regulation_data.config
+            self.regulation = regulation_data
+        elif isinstance(regulation_data, RegulationConfig):
+            self.config = regulation_data
+            self.regulation = None
+
+        # List of widgets for hiding / showing
         self.widgets: list[tuple[QLabel, QWidget]] = []
-        self.plan_regulation_name.setText(config.name)
+
+        # For accessing correct widgets when data is sent
+        self.value_widget = None
+        self.regulation_number_widget = None
+        self.additional_information_widgets: dict[str, QWidget | None] = {}  # Key = information type, value = widget
+        self.file_widgets: list[QgsFileWidget] = []
+        self.theme_widget = None
+        self.topic_tag_widget = None
+
+        self.expanded = True
+        self.plan_regulation_name.setText(regulation_data.name)
         self.plan_regulation_name.setReadOnly(True)
         self.del_btn.setIcon(QgsApplication.getThemeIcon("mActionDeleteSelected.svg"))
         self.del_btn.clicked.connect(lambda: self.delete_signal.emit(self))
         self.expand_hide_btn.clicked.connect(self._on_expand_hide_btn_clicked)
-        self.init_additional_information_btn()
-        self.init_other_information_btn()
+        self._init_additional_information_btn()
+        self._init_other_information_btn()
+        self._init_widgets()
 
-    @classmethod
-    def from_config(cls, config: PlanRegulationConfig, parent=None) -> PlanRegulationWidget:
-        instance = cls(config, parent)
-        instance.init_fields()
-        return instance
-
-    @classmethod
-    def from_definition(cls, definition: PlanRegulationDefinition, parent=None) -> PlanRegulationWidget:
-        instance = cls(definition.regulation_config, parent)
-        instance.init_fields_from_definition(definition)
-        return instance
-
-    def init_fields_from_definition(self, definition: PlanRegulationDefinition):
+    def _init_widgets(self):
         # Value input
         value_type = self.config.value_type
         if value_type:
-            self._add_value_input(value_type, self.config.unit, definition.default_value)
+            self._add_value_input(value_type, self.config.unit, self.regulation.value if self.regulation else None)
 
         # Additional information
-        for info in definition.additional_information:
-            info_type: str = cast("str", info["type"])
-            self.add_additional_info(info_type, info.get("value"))
-
-        # TODO: Other saved information from PlanRegulationDefinition
-
-    def init_fields(self):
-        value_type = self.config.value_type
-        if value_type:
-            self._add_value_input(value_type, self.config.unit)
-
-    @staticmethod
-    def _check_number_or_none(value: str | Number | None, error_msg: str):
-        if not isinstance(value, Number) and value is not None:
-            raise ValueError(error_msg)
+        if self.regulation:
+            for info_type, info_value in self.regulation.additional_information.items():
+                self._add_additional_info(info_type, info_value)
 
     def _add_value_input(
         self, value_type: ValueType, unit: str | None, default_value: str | Number | list[int] | None = None
@@ -122,42 +107,42 @@ class PlanRegulationWidget(QWidget, FormClass):  # type: ignore
         if value_type in [ValueType.DECIMAL, ValueType.POSITIVE_DECIMAL]:
             if not isinstance(default_value, Number) and default_value is not None:
                 raise ValueError(base_error_msg)
-            self.add_decimal_input(value_type, unit, default_value)
+            self._add_decimal_input(value_type, unit, default_value)
         elif value_type == ValueType.POSITIVE_INTEGER:
             if not isinstance(default_value, int) and default_value is not None:
                 raise ValueError(base_error_msg)
-            self.add_positive_integer_input(unit, default_value)
+            self._add_integer_input(value_type, unit, default_value)
         elif value_type == ValueType.POSITIVE_INTEGER_RANGE:
             if not isinstance(default_value, list) and default_value is not None:
                 raise ValueError(base_error_msg)
             if isinstance(default_value, list) and len(default_value) != 2:  # noqa: PLR2004
                 error_msg = f"Invalid number of values in default value {type(default_value)}."
                 raise ValueError(error_msg)
-            self.add_positive_integer_range_input(unit, default_value)
+            self._add_integer_range_input(value_type, unit, default_value)
         elif value_type == ValueType.VERSIONED_TEXT:
             if not isinstance(default_value, str) and default_value is not None:
                 raise ValueError(base_error_msg)
-            self.add_versioned_text_input(default_value)
+            self._add_versioned_text_input(default_value)
         else:
             msg = f"Invalid input value type for plan regulation: {value_type}"
             raise ValueError(msg)
 
-    def init_additional_information_btn(self):
+    def _init_additional_information_btn(self):
         informations_dict: dict[str, QMenu] = {}
         add_later: dict[str, list[str]] = {}
 
         def _add_action(informations_dict: dict[str, QMenu], parent_id: str, info_type: str):
             action = informations_dict[parent_id].addAction(info_type)
-            action.triggered.connect(lambda _: self.add_additional_info(info_type))
+            action.triggered.connect(lambda _: self._add_additional_info(info_type))
 
         # Iterate code layer and build menus
         for feature in get_layer_by_name("Lisätiedonlaji").getFeatures():
             if feature["level"] == 1:
-                menu = QMenu(feature["name"][self.language], self)
+                menu = QMenu(feature["name"][LANGUAGE], self)
                 informations_dict[feature["id"]] = menu
             else:
                 parent_id = feature["parent_id"]
-                info_type = feature["name"][self.language]
+                info_type = feature["name"][LANGUAGE]
                 if parent_id in informations_dict:
                     _add_action(informations_dict, parent_id, info_type)
                 else:
@@ -175,17 +160,17 @@ class PlanRegulationWidget(QWidget, FormClass):  # type: ignore
         self.add_additional_information_btn.setMenu(additional_information_type_menu)
         self.add_additional_information_btn.setIcon(QgsApplication.getThemeIcon("mActionPropertiesWidget.svg"))
 
-    def init_other_information_btn(self):
+    def _init_other_information_btn(self):
         add_field_menu = QMenu(self)
-        add_field_menu.addAction("Määräysnumero").triggered.connect(self.add_regulation_number)
-        add_field_menu.addAction("Liiteasiakirja").triggered.connect(self.add_file)
-        add_field_menu.addAction("Aihetunniste").triggered.connect(self.add_topic_tag)
+        add_field_menu.addAction("Määräysnumero").triggered.connect(self._add_regulation_number)
+        add_field_menu.addAction("Liiteasiakirja").triggered.connect(self._add_file)
+        add_field_menu.addAction("Aihetunniste").triggered.connect(self._add_topic_tag)
 
         theme_menu = QMenu("Kaavoitusteema", self)
         for feature in get_layer_by_name("Kaavoitusteemat").getFeatures():
-            name = feature["name"][self.language]
+            name = feature["name"][LANGUAGE]
             action = theme_menu.addAction(name)
-            action.triggered.connect(lambda _, name=name: self.add_theme(name))
+            action.triggered.connect(lambda _, name=name: self._add_theme(name))
         add_field_menu.addMenu(theme_menu)
 
         self.add_field_btn.setMenu(add_field_menu)
@@ -208,103 +193,91 @@ class PlanRegulationWidget(QWidget, FormClass):  # type: ignore
             self.expand_hide_btn.setArrowType(Qt.ArrowType.UpArrow)
             self.expanded = True
 
-    def _add_widgets_to_form(self, label: QLabel, widget: QWidget):
+    def _add_widgets(self, label: QLabel, widget: QWidget):
         self.form_layout.addRow(label, widget)
         self.widgets.append((label, widget))
         if not self.expanded:
             self._on_expand_hide_btn_clicked()
 
-    def add_decimal_input(self, value_type: ValueType, unit: str | None, default_value: Number | None = None):
-        value_widget = QgsDoubleSpinBox()
-        label = QLabel("Arvo")
-        if value_type == ValueType.POSITIVE_DECIMAL:
-            value_widget.setMinimum(0.0)
-            label.setToolTip("Tyyppi: desimaali (positiivinen)")
-        else:
-            value_widget.setMinimum(-9999.9)
-            label.setToolTip("Tyyppi: desimaali")
-        value_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        if unit:
-            value_widget.setSuffix(f" {unit}")
-        if default_value:
-            value_widget.setValue(default_value)
-        self._add_widgets_to_form(label, value_widget)
+    def _add_decimal_input(self, value_type: ValueType, unit: str | None, default_value: Number | None = None):
+        positive = value_type == ValueType.POSITIVE_DECIMAL
+        self.value_widget = DecimalInputWidget(default_value, unit, positive)
+        self._add_widgets(QLabel("Arvo"), self.value_widget)
 
-    def add_positive_integer_input(self, unit: str | None, default_value: int | None = None):
-        value_widget = QgsSpinBox()
-        value_widget.setMinimum(0)
-        value_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        if unit:
-            value_widget.setSuffix(f" {unit}")
-        label = QLabel("Arvo")
-        label.setToolTip("Tyyppi: kokonaisluku (positiivinen)")
-        if default_value:
-            value_widget.setValue(default_value)
-        self._add_widgets_to_form(label, value_widget)
+    def _add_integer_input(self, value_type: ValueType, unit: str | None, default_value: int | None = None):
+        positive = value_type == ValueType.POSITIVE_INTEGER
+        self.value_widget = IntegerInputWidget(default_value=default_value, unit=unit, positive=positive)
+        self._add_widgets(QLabel("Arvo"), self.value_widget)
 
-    def add_positive_integer_range_input(self, unit: str | None, default_values: list[int] | None = None):
-        min_widget = QgsSpinBox()
-        min_widget.setMinimum(0)
-        min_label = QLabel("Arvo minimi")
-        min_label.setToolTip("Tyyppi: kokonaisluku arvoväli (positiivinen)")
+    def _add_integer_range_input(
+        self, value_type: ValueType, unit: str | None, default_value: tuple[int, int] | None = None
+    ):
+        # NOTE: There is no ValueType.INTEGER_RANGE currently, so is always positive
+        positive = value_type == ValueType.POSITIVE_INTEGER_RANGE
+        self.value_widget = IntegerRangeInputWidget(default_value, unit, positive)
+        self._add_widgets(QLabel("Arvo"), self.value_widget)
 
-        max_widget = QgsSpinBox()
-        max_widget.setMinimum(0)
-        max_label = QLabel("Arvo maksimi")
-        max_label.setToolTip("Tyyppi: kokonaisluku arvoväli (positiivinen)")
-        if unit:
-            min_widget.setSuffix(f" {unit}")
-            max_widget.setSuffix(f" {unit}")
-        if default_values:
-            min_widget.setValue(default_values[0])
-            max_widget.setValue(default_values[1])
-        self._add_widgets_to_form(min_label, min_widget)
-        self._add_widgets_to_form(max_label, max_widget)
+    def _add_versioned_text_input(self, default_value: str | None = None):
+        self.value_widget = MultilineTextInputWidget(default_value=default_value, editable=False)
+        self._add_widgets(QLabel("Arvo"), self.value_widget)
 
-    def add_versioned_text_input(self, default_value: str | None = None):
-        text_widget = QTextEdit()
-        label = QLabel("Arvo")
-        label.setToolTip("Tyyppi: kieliversioitu teksti")
-        if default_value:
-            text_widget.setText(default_value)
-        self._add_widgets_to_form(label, text_widget)
-
-    def add_additional_info(self, info_type: str, default_value: str | Number | None = None):
+    def _add_additional_info(self, info_type: str, default_value: str | Number | None = None):
+        # TODO !
         # NOTE: Now info type is the name / readable version when this is triggered by user
         # Might need to refactor this later..
         name = get_additional_information_name(info_type)
-        info_type_line = QLineEdit(name)
-        info_type_line.setReadOnly(True)
-        label = QLabel("Lisätiedonlaji")
-        self._add_widgets_to_form(label, info_type_line)
+        self._add_widgets(QLabel("Lisätiedonlaji"), SinglelineTextInputWidget(name, False))
 
+        # NOTE: Does not support multiple instances of same additional information kind,
+        # for example if multiple Käyttötarkoituskohdistus are added, they overwrite each other
+        value_widget = None
         if name == "Käyttötarkoituskohdistus":
-            info_value_widget = QLineEdit()
-            info_value_label = QLabel(name)
-            self._add_widgets_to_form(info_value_label, info_value_widget)
+            value_widget = SinglelineTextInputWidget(default_value, True)
+            self._add_widgets(QLabel(name), value_widget)
 
-            if default_value:
-                info_value_widget.setText(str(default_value))
+        self.additional_information_widgets[info_type] = value_widget
 
-    def add_regulation_number(self):
-        if not self.regulation_number_added:
-            number_widget = QgsSpinBox()
-            label = QLabel("Määräysnumero")
-            self._add_widgets_to_form(label, number_widget)
-            self.regulation_number_added = True
+    def _add_regulation_number(self):
+        if not self.regulation_number_widget:
+            self.regulation_number_widget = IntegerInputWidget(None, None, True)
+            self._add_widgets(QLabel("Määräysnumero"), self.regulation_number_widget)
 
-    def add_file(self):
-        file_input = QgsFileWidget()
-        label = QLabel("Liiteasiakirja")
-        self._add_widgets_to_form(label, file_input)
+    def _add_file(self):
+        widget = QgsFileWidget()
+        self._add_widgets(QLabel("Liiteasiakirja"), widget)
+        self.file_widgets.append(widget)
 
-    def add_topic_tag(self):
-        text_input = QLineEdit()
-        label = QLabel("Aihetunniste")
-        self._add_widgets_to_form(label, text_input)
+    def _add_topic_tag(self):
+        self.topic_tag_widget = SinglelineTextInputWidget(None, True)
+        self._add_widgets(QLabel("Aihetunniste"), self.topic_tag_widget)
 
-    def add_theme(self, theme_name: str):
-        theme_type_line = QLineEdit(theme_name)
-        theme_type_line.setReadOnly(True)
-        label = QLabel("Kaavoitusteema")
-        self._add_widgets_to_form(label, theme_type_line)
+    def _add_theme(self, theme_name: str):
+        self.theme_widget = SinglelineTextInputWidget(theme_name, False)
+        self._add_widgets(QLabel("Kaavoitusteema"), self.theme_widget)
+
+    # Or e.g. "into_regulation"
+    def into_model(self, regulation_group_id: str) -> Regulation:
+        return Regulation(
+            config=self.config,
+            value=self.value_widget.get_value(),
+            regulation_number=self.regulation_number_widget.get_value(),
+            additional_information={
+                name: widget.get_value() for name, widget in self.additional_information_widgets.items() if widget
+            },
+            files=[file.filePath() for file in self.file_widgets],
+            theme=self.theme_widget.get_value(),
+            topic_tag=self.topic_tag_widget.get_value(),
+            regulation_group_id_=regulation_group_id,
+            id_=self.regulation.id_ if self.regulation else None
+        )
+
+    def save_data(self, regulation_group_id: str) -> Regulation:
+        model = self.into_model(regulation_group_id)
+        regulation_feature = PlanRegulationLayer.feature_from_model(model)
+        layer = PlanRegulationLayer.get_from_project()
+        if model.id_ is None:
+            layer.addFeature(regulation_feature)
+        else:
+            layer.updateFeature(regulation_feature)
+
+        return model
