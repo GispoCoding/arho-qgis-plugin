cf4c3cf9928cd6c0bf4eea1b6591095d082aa8c0
Miikka Kallio <miikka@gispo.fi>
2024-12-18 05:56:48 +0200

    Moved progress notifier to the main dialog.

--- arho_feature_template/gui/post_plan.py
+++ arho_feature_template/gui/post_plan.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 from importlib import resources
 from typing import TYPE_CHECKING
 
-from qgis.PyQt.QtWidgets import QDialog, QProgressDialog, QListView, QMessageBox
-from qgis.PyQt.QtCore import Qt, QStringListModel
 from qgis.PyQt import uic
+from qgis.PyQt.QtCore import QStringListModel
+from qgis.PyQt.QtWidgets import QDialog, QListView, QMessageBox, QProgressBar
 
 from arho_feature_template.core.lambda_service import LambdaService
 from arho_feature_template.utils.misc_utils import get_active_plan_id
@@ -28,35 +28,31 @@ class PostPlan(QDialog, FormClass):  # type: ignore
         self.validation_list_view: QListView = self.validationListView
         self.validation_list_view.setModel(self.validation_list_model)
 
+        # Initialize progress bar
+        self.progress_bar: QProgressBar = self.progressBar
+        self.progress_bar.setRange(0, 0)  # Set to "busy" indicator mode initially
+        self.progress_bar.hide()  # Hide initially
+
         # Initialize buttons
         self.export_button = self.exportButton
         self.export_button.clicked.connect(self.post_plan)
         self.dialogButtonBox.rejected.connect(self.reject)
 
-        # Setup progress dialog (initially hidden)
-        self.progress_dialog = QProgressDialog("Processing...", None, 0, 0, self)
-        self.progress_dialog.setWindowModality(Qt.WindowModal)
-        self.progress_dialog.setCancelButton(None)
-        self.progress_dialog.setMinimumDuration(0)
-        self.progress_dialog.hide()
-
     def post_plan(self):
         """Handles the 'Vie kaava' button logic."""
-        print("Starting export...")
-
         # Disable the button to prevent multiple clicks
         self.export_button.setEnabled(False)
-        self.progress_dialog.show()
+        self.progress_bar.show()
 
         plan_id = get_active_plan_id()
         self.lambda_service = LambdaService()
-        self.lambda_service.post_received.connect(self.handle_validation_errors)
+        self.lambda_service.post_received.connect(self.do_something)
         self.lambda_service.post_plan(plan_id)
 
-    def handle_validation_errors(self, post_json):
+    def do_something(self, post_json):
         """Handles the Lambda response."""
         if not post_json:
-            self.progress_dialog.hide()
+            self.progress_bar.hide()
             QMessageBox.critical(self, "Virhe", "Lambda palautti tyhjän vastauksen.")
             self.export_button.setEnabled(True)
             return
@@ -86,6 +82,6 @@ class PostPlan(QDialog, FormClass):  # type: ignore
         # Update the list view with errors and warnings
         self.validation_list_model.setStringList(new_errors)
 
-        # Hide progress dialog and re-enable the button
-        self.progress_dialog.hide()
+        # Hide progress bar and re-enable the button
+        self.progress_bar.hide()
         self.export_button.setEnabled(True)
