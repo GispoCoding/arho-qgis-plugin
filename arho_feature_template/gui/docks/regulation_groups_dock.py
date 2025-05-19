1984596d0d13d49a915e8915429124b7a5bb4943
Niko Aarnio <niko.aarnio@protonmail.com>
2025-05-19 14:39:30 +0300

    add possibility to select multiple regulation groups at a time
    Now many regulation grups can also be deleted at a time. Using text search no longer clears selection if search does not match selected groups.

--- arho_feature_template/gui/docks/regulation_groups_dock.py
+++ arho_feature_template/gui/docks/regulation_groups_dock.py
@@ -11,7 +11,7 @@ from qgis.PyQt.QtWidgets import QListWidget, QListWidgetItem, QMessageBox, QPush
 
 from arho_feature_template.core.models import RegulationGroup, RegulationGroupLibrary
 from arho_feature_template.project.layers.plan_layers import plan_feature_layers
-from arho_feature_template.utils.misc_utils import disconnect_signal
+from arho_feature_template.utils.misc_utils import disconnect_signal, iface
 
 if TYPE_CHECKING:
     from qgis.gui import QgsFilterLineEdit
@@ -23,39 +23,38 @@ DockClass, _ = uic.loadUiType(ui_path)
 
 
 class RegulationGroupsDock(QgsDockWidget, DockClass):  # type: ignore
-    search_box: QgsFilterLineEdit
-    regulation_group_list: QListWidget
-    dockWidgetContents: QWidget  # noqa: N815
-
-    new_btn: QPushButton
-    delete_btn: QPushButton
-    edit_btn: QPushButton
-
-    new_regulation_group_requested = pyqtSignal()
-    edit_regulation_group_requested = pyqtSignal(RegulationGroup)
-    delete_regulation_group_requested = pyqtSignal(RegulationGroup)
-    delete_all_regulation_groups_requested = pyqtSignal(object)
-    add_regulation_groups_for_all_requested = pyqtSignal(object, object)
+    request_new_regulation_group = pyqtSignal()
+    request_edit_regulation_group = pyqtSignal(RegulationGroup)
+    request_delete_regulation_groups = pyqtSignal(object)  # Type: list[RegulationGroup]
+    request_delete_all_regulation_groups = pyqtSignal(object)  # Type: list[tuple[str, Generator[str]]]
+    request_add_groups_to_feats = pyqtSignal(
+        object, object
+    )  # Types: list[RegulationGroup],  list[tuple[str, Generator[str]]
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
 
+        # TYPES
+        self.search_box: QgsFilterLineEdit
+        self.dockWidgetContents: QWidget
+        self.regulation_group_list: QListWidget
+
+        self.new_btn: QPushButton
+        self.delete_btn: QPushButton
+        self.edit_btn: QPushButton
+        self.remove_all_btn: QPushButton
+        self.add_all_btn: QPushButton
+
+        # INIT
         self.new_btn.setIcon(QgsApplication.getThemeIcon("mActionAdd.svg"))
         self.delete_btn.setIcon(QgsApplication.getThemeIcon("mActionDeleteSelected.svg"))
         self.edit_btn.setIcon(QgsApplication.getThemeIcon("mActionEditTable.svg"))
-        self.remove_all_btn: QPushButton
-        self.add_all_btn: QPushButton
-        # QgsApplication.getThemeIcon("symbologyEdit.svg")
 
-        self.connect_buttons()
-
-        self.regulation_group_list.setSelectionMode(self.regulation_group_list.SingleSelection)
-        self.regulation_group_list.clicked.connect(self.on_regulation_group_item_clicked)
-
+        self.regulation_group_list.setSelectionMode(self.regulation_group_list.ExtendedSelection)
         self.search_box.valueChanged.connect(self.filter_regulation_groups)
 
-        self.selected_group: RegulationGroup | None = None
+        self.connect_buttons()
 
     def _disconnect_buttons(self):
         disconnect_signal(self.new_btn.clicked)
@@ -67,7 +66,7 @@ class RegulationGroupsDock(QgsDockWidget, DockClass):  # type: ignore
     def connect_buttons(self):
         self._disconnect_buttons()
 
-        self.new_btn.clicked.connect(self.new_regulation_group_requested.emit)
+        self.new_btn.clicked.connect(self.request_new_regulation_group.emit)
         self.edit_btn.clicked.connect(self.on_edit_btn_clicked)
         self.delete_btn.clicked.connect(self.on_delete_btn_clicked)
         self.remove_all_btn.clicked.connect(self.on_remove_all_btn_clicked)
@@ -80,8 +79,6 @@ class RegulationGroupsDock(QgsDockWidget, DockClass):  # type: ignore
             for group in category.regulation_groups:
                 self.add_regulation_group_to_list(group)
 
-        self.selected_group = None
-
     def add_regulation_group_to_list(self, group: RegulationGroup):
         text = str(group)
         item = QListWidgetItem(text)
@@ -89,26 +86,19 @@ class RegulationGroupsDock(QgsDockWidget, DockClass):  # type: ignore
         item.setData(Qt.UserRole, group)
         self.regulation_group_list.addItem(item)
 
-    def on_regulation_group_item_clicked(self, index: int):
-        item = self.regulation_group_list.itemFromIndex(index)
-        group: RegulationGroup = item.data(Qt.UserRole)
-        # Clicked new list item => activate new group
-        if group != self.selected_group:
-            self.selected_group = group
-            # NOTE: Workaround to fix make sure clicked item is selected. At least after a selected item has been
-            # filtered out, clicking on a new template will select, immediately deselect (but keep focus)
-            self.regulation_group_list.setCurrentItem(item)
-        else:
-            # Clicked selected list item => clear selection
-            self.selected_group = None
-            self.regulation_group_list.clearSelection()
+    def get_selected_regulation_groups(self) -> list[RegulationGroup]:
+        return [item.data(Qt.UserRole) for item in self.regulation_group_list.selectedItems()]
 
     def on_edit_btn_clicked(self):
-        if self.selected_group:
-            self.edit_regulation_group_requested.emit(self.selected_group)
+        selected = self.get_selected_regulation_groups()
+        if len(selected) == 1:
+            self.request_edit_regulation_group.emit(selected[0])
+        else:
+            iface.messageBar().pushWarning("", "Valitse vain yksi kaavamääräysryhmä kerrallaan muokkaamista varten.")
 
     def on_delete_btn_clicked(self):
-        if self.selected_group:
+        selected_groups = self.get_selected_regulation_groups()
+        if len(selected_groups) > 0:
             response = QMessageBox.question(
                 None,
                 "Kaavamääräysryhmän poisto",
@@ -116,39 +106,33 @@ class RegulationGroupsDock(QgsDockWidget, DockClass):  # type: ignore
                 QMessageBox.Yes | QMessageBox.No,
             )
             if response == QMessageBox.Yes:
-                self.delete_regulation_group_requested.emit(self.selected_group)
+                self.request_delete_regulation_groups.emit(selected_groups)
 
     def on_remove_all_btn_clicked(self):
         feats: list[tuple[str, Generator[str]]] = [
             (layer_class.name, layer_class.get_selected_feature_ids()) for layer_class in plan_feature_layers
         ]
-        self.delete_all_regulation_groups_requested.emit(feats)
+        self.request_delete_all_regulation_groups.emit(feats)
 
     def on_add_all_btn_clicked(self):
-        feats: list[tuple[str, Generator[str]]] = [
-            (layer_class.name, layer_class.get_selected_feature_ids()) for layer_class in plan_feature_layers
-        ]
-        if self.selected_group:
-            selected_groups = [self.selected_group]
-            self.add_regulation_groups_for_all_requested.emit(selected_groups, feats)
+        selected_groups = self.get_selected_regulation_groups()
+        if len(selected_groups) > 0:
+            feats: list[tuple[str, Generator[str]]] = [
+                (layer_class.name, layer_class.get_selected_feature_ids()) for layer_class in plan_feature_layers
+            ]
+            self.request_add_groups_to_feats.emit(selected_groups, feats)
 
     def filter_regulation_groups(self) -> None:
         search_text = self.search_box.value().lower()
-
         for index in range(self.regulation_group_list.count()):
             item = self.regulation_group_list.item(index)
-            regulation_group: RegulationGroup = item.data(Qt.UserRole)
             item.setHidden(search_text not in item.text().lower())
-
-            # Clear selection if the selected template was filtered
-            if self.selected_group and self.selected_group is regulation_group and item.isHidden():
-                self.regulation_group_list.clearSelection()
 
     def unload(self):
         self._disconnect_buttons()
 
-        disconnect_signal(self.new_regulation_group_requested)
-        disconnect_signal(self.edit_regulation_group_requested)
-        disconnect_signal(self.delete_regulation_group_requested)
-        disconnect_signal(self.delete_all_regulation_groups_requested)
-        disconnect_signal(self.add_regulation_groups_for_all_requested)
+        disconnect_signal(self.request_new_regulation_group)
+        disconnect_signal(self.request_edit_regulation_group)
+        disconnect_signal(self.request_delete_regulation_groups)
+        disconnect_signal(self.request_delete_all_regulation_groups)
+        disconnect_signal(self.request_add_groups_to_feats)
