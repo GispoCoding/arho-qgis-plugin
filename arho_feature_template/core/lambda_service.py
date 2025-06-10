GispoCoding/arho-feature-template

Add button for exporting plan matter as JSON
Combined JSON export buttons under one QMenu

arho_feature_template/core/lambda_service.py
@@ -14,6 +14,7 @@ from arho_feature_template.utils.misc_utils import get_active_plan_id, get_setti
 
 
 class LambdaService(QObject):
+    json_received = pyqtSignal(dict)
     jsons_received = pyqtSignal(dict, dict)
     validation_received = pyqtSignal(dict)
     validation_failed = pyqtSignal()
@@ -24,6 +25,7 @@ class LambdaService(QObject):
     ACTION_VALIDATE_PLANS = "validate_plans"
     ACTION_VALIDATE_PLAN_MATTERS = "validate_plan_matters"
     ACTION_GET_PLANS = "get_plans"
+    ACTION_GET_PLAN_MATTERS = "get_plan_matters"
     ACTION_POST_PLAN_MATTERS = "post_plan_matters"
     ACTION_GET_PERMANENT_IDENTIFIERS = "get_permanent_plan_identifiers"
 
@@ -34,6 +36,9 @@ class LambdaService(QObject):
 
     def serialize_plan(self, plan_id: str):
         self._send_request(action=self.ACTION_GET_PLANS, plan_id=plan_id)
+
+    def serialize_plan_matter(self, plan_id: str):
+        self._send_request(action=self.ACTION_GET_PLAN_MATTERS, plan_id=plan_id)
 
     def validate_plan(self, plan_id: str):
         self._send_request(action=self.ACTION_VALIDATE_PLANS, plan_id=plan_id)
@@ -76,7 +81,8 @@ class LambdaService(QObject):
 
     def _get_response_handler(self, action: str) -> Callable[[dict], None]:
         handlers = {
-            self.ACTION_GET_PLANS: self._process_json_reply,
+            self.ACTION_GET_PLANS: self._process_plan_json_reply,
+            self.ACTION_GET_PLAN_MATTERS: self._process_plan_matter_json_reply,
             self.ACTION_VALIDATE_PLANS: self._process_validation_reply,
             self.ACTION_VALIDATE_PLAN_MATTERS: self._process_validation_reply,
             self.ACTION_POST_PLAN_MATTERS: self._process_plan_matter_reply,
@@ -87,6 +93,7 @@ class LambdaService(QObject):
     def _get_error_handler(self, action: str) -> Callable[[], None]:
         handlers = {
             self.ACTION_GET_PLANS: lambda: None,
+            self.ACTION_GET_PLAN_MATTERS: lambda: None,
             self.ACTION_VALIDATE_PLANS: self._handle_validation_error,
             self.ACTION_VALIDATE_PLAN_MATTERS: self._handle_validation_error,
             self.ACTION_POST_PLAN_MATTERS: lambda: None,
@@ -162,7 +169,7 @@ class LambdaService(QObject):
         validation_errors = response_json.get("ryhti_responses")
         self.validation_received.emit(validation_errors)
 
-    def _process_json_reply(self, response_json: dict):
+    def _process_plan_json_reply(self, response_json: dict):
         """Processes the reply from the lambda and emits signal."""
         plan_id = get_active_plan_id()
 
@@ -184,3 +191,17 @@ class LambdaService(QObject):
 
         # Emit the signal with the two JSONs
         self.jsons_received.emit(plan_json, outline_json)
+
+    def _process_plan_matter_json_reply(self, response_json: dict):
+        """Processes the reply from the lambda and emits signal."""
+        plan_id = get_active_plan_id()
+
+        details = response_json.get("details", {})
+
+        # Extract the plan matter JSON for the given plan_id
+        plan_matter_json = details.get(plan_id, {})
+        if not isinstance(plan_matter_json, dict):
+            plan_matter_json = {}
+
+        # Emit the signal with the JSON
+        self.json_received.emit(plan_matter_json)

arho_feature_template/core/plan_manager.py
@@ -30,6 +30,7 @@ from arho_feature_template.gui.dialogs.plan_attribute_form import PlanAttributeF
 from arho_feature_template.gui.dialogs.plan_feature_form import PlanFeatureForm
 from arho_feature_template.gui.dialogs.plan_regulation_group_form import PlanRegulationGroupForm
 from arho_feature_template.gui.dialogs.serialize_plan import SerializePlan
+from arho_feature_template.gui.dialogs.serialize_plan_matter import SerializePlanMatter
 from arho_feature_template.gui.docks.new_feature_dock import NewFeatureDock
 from arho_feature_template.gui.docks.regulation_groups_dock import RegulationGroupsDock
 from arho_feature_template.gui.tools.inspect_plan_features_tool import InspectPlanFeatures
@@ -89,6 +90,7 @@ class PlanManager(QObject):
         super().__init__()
         self.json_plan_path = None
         self.json_plan_outline_path = None
+        self.json_plan_matter_path = None
 
         self.feature_template_libraries = []
         self.regulation_group_libraries = []
@@ -133,6 +135,7 @@ class PlanManager(QObject):
             lambda value: self.set_permanent_identifier(value["identifier"])
         )
         self.lambda_service.jsons_received.connect(self.save_plan_jsons)
+        self.lambda_service.json_received.connect(self.save_plan_matter_json)
 
     def initialize_from_project(self):
         # # If project is not open, don't try to initialize
@@ -489,6 +492,19 @@ class PlanManager(QObject):
 
             self.lambda_service.serialize_plan(plan_id)
 
+    def get_plan_matter_json(self):
+        """Serializes plan matter to JSON"""
+        plan_id = get_active_plan_id()
+        if not plan_id:
+            iface.messageBar().pushWarning("", "Mikään kaava ei ole avattuna.")
+            return
+
+        dialog = SerializePlanMatter()
+        if dialog.exec_() == QDialog.Accepted:
+            self.json_plan_matter_path = str(dialog.plan_matter_file.filePath())
+
+            self.lambda_service.serialize_plan_matter(plan_id)
+
     def get_permanent_plan_identifier(self):
         """Gets the permanent plan identifier for the active plan."""
         plan_id = get_active_plan_id()
@@ -527,6 +543,23 @@ class PlanManager(QObject):
             json.dump(outline_json, outline_file, ensure_ascii=False, indent=2)
 
         iface.messageBar().pushSuccess("", "Kaava ja kaavan ulkoraja tallennettu.")
+
+    def save_plan_matter_json(self, plan_matter_json):
+        """This slot saves the plan matter JSON to file."""
+        if plan_matter_json is None:
+            iface.messageBar().pushCritical("", "Kaava-asiaa ei löytynyt.")
+            return
+
+        # Retrieve path
+        if self.json_plan_matter_path is None:
+            iface.messageBar().pushCritical("", "Tiedostopolku ei ole saatavilla.")
+            return
+
+        # Save the JSON
+        with open(self.json_plan_matter_path, "w", encoding="utf-8") as file:
+            json.dump(plan_matter_json, file, ensure_ascii=False, indent=2)
+
+        iface.messageBar().pushSuccess("", "Kaava-asia tallennettu.")
 
     def on_project_loaded(self):
         self.initialize_from_project()

GispoCoding/arho-qgis-plugin

arho_feature_template/core/lambda_service.py
@@ -14,7 +14,8 @@ from arho_feature_template.utils.misc_utils import get_active_plan_id, get_setti
 
 
 class LambdaService(QObject):
-    jsons_received = pyqtSignal(dict, dict)
+    plan_matter_json_received = pyqtSignal(dict)
+    plan_jsons_received = pyqtSignal(dict, dict)
     validation_received = pyqtSignal(dict)
     validation_failed = pyqtSignal()
     plan_matter_received = pyqtSignal(dict)
@@ -24,6 +25,7 @@ class LambdaService(QObject):
     ACTION_VALIDATE_PLANS = "validate_plans"
     ACTION_VALIDATE_PLAN_MATTERS = "validate_plan_matters"
     ACTION_GET_PLANS = "get_plans"
+    ACTION_GET_PLAN_MATTERS = "get_plan_matters"
     ACTION_POST_PLAN_MATTERS = "post_plan_matters"
     ACTION_GET_PERMANENT_IDENTIFIERS = "get_permanent_plan_identifiers"
 
@@ -34,6 +36,9 @@ class LambdaService(QObject):
 
     def serialize_plan(self, plan_id: str):
         self._send_request(action=self.ACTION_GET_PLANS, plan_id=plan_id)
+
+    def serialize_plan_matter(self, plan_id: str):
+        self._send_request(action=self.ACTION_GET_PLAN_MATTERS, plan_id=plan_id)
 
     def validate_plan(self, plan_id: str):
         self._send_request(action=self.ACTION_VALIDATE_PLANS, plan_id=plan_id)
@@ -76,7 +81,8 @@ class LambdaService(QObject):
 
     def _get_response_handler(self, action: str) -> Callable[[dict], None]:
         handlers = {
-            self.ACTION_GET_PLANS: self._process_json_reply,
+            self.ACTION_GET_PLANS: self._process_plan_json_reply,
+            self.ACTION_GET_PLAN_MATTERS: self._process_plan_matter_json_reply,
             self.ACTION_VALIDATE_PLANS: self._process_validation_reply,
             self.ACTION_VALIDATE_PLAN_MATTERS: self._process_validation_reply,
             self.ACTION_POST_PLAN_MATTERS: self._process_plan_matter_reply,
@@ -87,6 +93,7 @@ class LambdaService(QObject):
     def _get_error_handler(self, action: str) -> Callable[[], None]:
         handlers = {
             self.ACTION_GET_PLANS: lambda: None,
+            self.ACTION_GET_PLAN_MATTERS: lambda: None,
             self.ACTION_VALIDATE_PLANS: self._handle_validation_error,
             self.ACTION_VALIDATE_PLAN_MATTERS: self._handle_validation_error,
             self.ACTION_POST_PLAN_MATTERS: lambda: None,
@@ -162,7 +169,7 @@ class LambdaService(QObject):
         validation_errors = response_json.get("ryhti_responses")
         self.validation_received.emit(validation_errors)
 
-    def _process_json_reply(self, response_json: dict):
+    def _process_plan_json_reply(self, response_json: dict):
         """Processes the reply from the lambda and emits signal."""
         plan_id = get_active_plan_id()
 
@@ -183,4 +190,18 @@ class LambdaService(QObject):
                 }
 
         # Emit the signal with the two JSONs
-        self.jsons_received.emit(plan_json, outline_json)
+        self.plan_jsons_received.emit(plan_json, outline_json)
+
+    def _process_plan_matter_json_reply(self, response_json: dict):
+        """Processes the reply from the lambda and emits signal."""
+        plan_id = get_active_plan_id()
+
+        details = response_json.get("details", {})
+
+        # Extract the plan matter JSON for the given plan_id
+        plan_matter_json = details.get(plan_id, {})
+        if not isinstance(plan_matter_json, dict):
+            plan_matter_json = {}
+
+        # Emit the signal with the JSON
+        self.plan_matter_json_received.emit(plan_matter_json)

arho_feature_template/core/plan_manager.py
@@ -30,6 +30,7 @@ from arho_feature_template.gui.dialogs.plan_attribute_form import PlanAttributeF
 from arho_feature_template.gui.dialogs.plan_feature_form import PlanFeatureForm
 from arho_feature_template.gui.dialogs.plan_regulation_group_form import PlanRegulationGroupForm
 from arho_feature_template.gui.dialogs.serialize_plan import SerializePlan
+from arho_feature_template.gui.dialogs.serialize_plan_matter import SerializePlanMatter
 from arho_feature_template.gui.docks.new_feature_dock import NewFeatureDock
 from arho_feature_template.gui.docks.regulation_groups_dock import RegulationGroupsDock
 from arho_feature_template.gui.tools.inspect_plan_features_tool import InspectPlanFeatures
@@ -89,6 +90,7 @@ class PlanManager(QObject):
         super().__init__()
         self.json_plan_path = None
         self.json_plan_outline_path = None
+        self.json_plan_matter_path = None
 
         self.feature_template_libraries = []
         self.regulation_group_libraries = []
@@ -132,7 +134,8 @@ class PlanManager(QObject):
         self.lambda_service.plan_identifier_received.connect(
             lambda value: self.set_permanent_identifier(value["identifier"])
         )
-        self.lambda_service.jsons_received.connect(self.save_plan_jsons)
+        self.lambda_service.plan_jsons_received.connect(self.save_plan_jsons)
+        self.lambda_service.plan_matter_json_received.connect(self.save_plan_matter_json)
 
     def initialize_from_project(self):
         # # If project is not open, don't try to initialize
@@ -489,6 +492,19 @@ class PlanManager(QObject):
 
             self.lambda_service.serialize_plan(plan_id)
 
+    def get_plan_matter_json(self):
+        """Serializes plan matter to JSON"""
+        plan_id = get_active_plan_id()
+        if not plan_id:
+            iface.messageBar().pushWarning("", "Mikään kaava ei ole avattuna.")
+            return
+
+        dialog = SerializePlanMatter()
+        if dialog.exec_() == QDialog.Accepted:
+            self.json_plan_matter_path = str(dialog.plan_matter_file.filePath())
+
+            self.lambda_service.serialize_plan_matter(plan_id)
+
     def get_permanent_plan_identifier(self):
         """Gets the permanent plan identifier for the active plan."""
         plan_id = get_active_plan_id()
@@ -528,6 +544,23 @@ class PlanManager(QObject):
 
         iface.messageBar().pushSuccess("", "Kaava ja kaavan ulkoraja tallennettu.")
 
+    def save_plan_matter_json(self, plan_matter_json):
+        """This slot saves the plan matter JSON to file."""
+        if plan_matter_json is None:
+            iface.messageBar().pushCritical("", "Kaava-asiaa ei löytynyt.")
+            return
+
+        # Retrieve path
+        if self.json_plan_matter_path is None:
+            iface.messageBar().pushCritical("", "Tiedostopolku ei ole saatavilla.")
+            return
+
+        # Save the JSON
+        with open(self.json_plan_matter_path, "w", encoding="utf-8") as file:
+            json.dump(plan_matter_json, file, ensure_ascii=False, indent=2)
+
+        iface.messageBar().pushSuccess("", "Kaava-asia tallennettu.")
+
     def on_project_loaded(self):
         self.initialize_from_project()
 
@@ -549,7 +582,8 @@ class PlanManager(QObject):
         iface.actionPan().trigger()
 
         # Lambda service
-        disconnect_signal(self.lambda_service.jsons_received)
+        disconnect_signal(self.lambda_service.plan_jsons_received)
+        disconnect_signal(self.lambda_service.plan_matter_json_received)
         self.lambda_service.deleteLater()
 
         # Feature digitize tool