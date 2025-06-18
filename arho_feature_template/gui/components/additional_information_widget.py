d8489148e0c93bbf136f08f7d7fd3183b498a1ae
Niko Aarnio <niko.aarnio@protonmail.com>
2025-06-18 11:27:07 +0300

    show additional information widget directly under regulation widget, refactor additional information widget code

--- arho_feature_template/gui/components/additional_information_widget.py
+++ arho_feature_template/gui/components/additional_information_widget.py
@@ -6,15 +6,9 @@ from typing import TYPE_CHECKING
 from qgis.core import QgsApplication
 from qgis.PyQt import uic
 from qgis.PyQt.QtCore import pyqtSignal
-from qgis.PyQt.QtWidgets import (
-    QFormLayout,
-    QLabel,
-    QLineEdit,
-    QWidget,
-)
+from qgis.PyQt.QtWidgets import QLabel, QLineEdit, QWidget
 
-from arho_feature_template.core.models import AdditionalInformation, AttributeValue
-from arho_feature_template.gui.components.required_field_label import RequiredFieldLabel
+from arho_feature_template.core.models import AdditionalInformation
 from arho_feature_template.gui.components.value_input_widgets import (
     ValueWidgetManager,
 )
@@ -27,12 +21,7 @@ FormClass, _ = uic.loadUiType(ui_path)
 
 
 class AdditionalInformationWidget(QWidget, FormClass):  # type: ignore
-    """A widget representation of a plan regulation."""
-
-    # TYPES
-    type_of_additional_information_name: QLineEdit
-    form_layout: QFormLayout
-    del_btn: QPushButton
+    """A widget representation of an additional information of plan regulation."""
 
     delete_signal = pyqtSignal(QWidget)
 
@@ -40,38 +29,31 @@ class AdditionalInformationWidget(QWidget, FormClass):  # type: ignore
         super().__init__(parent)
         self.setupUi(self)
 
+        # TYPES
+        self.additional_info: QLineEdit
+        self.del_btn: QPushButton
+
         # INIT
-        self.config = additional_information.config
-        self.additional_information = additional_information
+        self.from_model(additional_information)
 
-        self.value_widget_manager = (
-            ValueWidgetManager(
-                self.additional_information.value,
-                self.config.default_value,
-            )
-            if self.config and self.config.default_value
-            else None
-        )
-
-        # List of widgets for hiding / showing
-        self.widgets: list[tuple[QLabel, QWidget]] = []
-
-        self.type_of_additional_information_name.setText(self.config.name)
-        self.type_of_additional_information_name.setReadOnly(True)
         self.del_btn.setIcon(QgsApplication.getThemeIcon("mActionDeleteSelected.svg"))
         self.del_btn.clicked.connect(lambda: self.delete_signal.emit(self))
 
-        if self.value_widget_manager is not None:
+    def from_model(self, additional_information: AdditionalInformation):
+        self.additional_information = additional_information
+        self.config = self.additional_information.config
+        self.additional_info.setText(additional_information.config.name)
+
+        self.value_widget_manager: ValueWidgetManager | None = None
+
+        if self.config and self.config.default_value:
+            self.value_widget_manager = ValueWidgetManager(self.additional_information.value, self.config.default_value)
             widget = (
                 self.value_widget_manager.value_widget
                 if self.value_widget_manager.value_widget is not None
                 else QLabel("Syötekenttää tälle tyypille ei ole vielä toteutettu")
             )
-            self._add_widget(RequiredFieldLabel("Arvo:"), widget)
-
-    def _add_widget(self, label: QLabel, widget: QWidget):
-        self.form_layout.addRow(label, widget)
-        self.widgets.append((label, widget))
+            self.layout().addWidget(widget)
 
     def into_model(self) -> AdditionalInformation:
         model = AdditionalInformation(
@@ -80,7 +62,7 @@ class AdditionalInformationWidget(QWidget, FormClass):  # type: ignore
             plan_regulation_id=self.additional_information.plan_regulation_id,
             type_additional_information_id=self.additional_information.type_additional_information_id,
             modified=self.additional_information.modified,
-            value=self.value_widget_manager.into_model() if self.value_widget_manager else AttributeValue(),
+            value=self.value_widget_manager.into_model() if self.value_widget_manager else None,
         )
         if not model.modified and model != self.additional_information:
             model.modified = True
