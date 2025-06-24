GispoCoding/arho-feature-template

implement custom/user regulation group template libraries - added from and into template dict methods for relevant models - adjusted logic for existing template reading - disabled reading of the handwritten katja regulation group template config files as they are now incompatible - added a couple convienent ID methods for layer classes - created TemplateManager static class for handling template configs – could be changed into 'template_utils' or similar

arho_feature_template/gui/dialogs/plan_regulation_group_form.py
@@ -50,7 +50,9 @@ class PlanRegulationGroupForm(QDialog, FormClass):  # type: ignore
     """Form to create a new plan regulation group."""
 
     def __init__(
-        self, regulation_group: RegulationGroup, active_plan_regulation_groups_library: RegulationGroupLibrary
+        self,
+        regulation_group: RegulationGroup,
+        active_plan_regulation_groups_library: RegulationGroupLibrary | None,
     ):
         super().__init__()
         self.setupUi(self)
@@ -90,8 +92,11 @@ class PlanRegulationGroupForm(QDialog, FormClass):  # type: ignore
         self.regulation_group = regulation_group
         self.regulation_widgets: list[RegulationWidget] = []
         self.proposition_widgets: list[PropositionWidget] = []
-        self.save_as_config = False
-        self.existing_group_short_names = active_plan_regulation_groups_library.get_short_names()
+
+        if active_plan_regulation_groups_library:
+            self.existing_group_short_names = active_plan_regulation_groups_library.get_short_names()
+        else:
+            self.existing_group_short_names = set()
 
         # Initialize regulation library
         self.regulations_selection_widget = TreeWithSearchWidget()

GispoCoding/arho-qgis-plugin

arho_feature_template/gui/dialogs/plan_regulation_group_form.py
@@ -50,7 +50,9 @@ class PlanRegulationGroupForm(QDialog, FormClass):  # type: ignore
     """Form to create a new plan regulation group."""
 
     def __init__(
-        self, regulation_group: RegulationGroup, active_plan_regulation_groups_library: RegulationGroupLibrary
+        self,
+        regulation_group: RegulationGroup,
+        active_plan_regulation_groups_library: RegulationGroupLibrary | None,
     ):
         super().__init__()
         self.setupUi(self)
@@ -90,8 +92,11 @@ class PlanRegulationGroupForm(QDialog, FormClass):  # type: ignore
         self.regulation_group = regulation_group
         self.regulation_widgets: list[RegulationWidget] = []
         self.proposition_widgets: list[PropositionWidget] = []
-        self.save_as_config = False
-        self.existing_group_letter_codes = active_plan_regulation_groups_library.get_letter_codes()
+
+        if active_plan_regulation_groups_library:
+            self.existing_group_letter_codes = active_plan_regulation_groups_library.get_letter_codes()
+        else:
+            self.existing_group_letter_codes = set()
 
         # Initialize regulation library
         self.regulations_selection_widget = TreeWithSearchWidget()