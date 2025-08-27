dc2b6c8bbeaefc4cfba270c03324f74046e80cb2
mipeso <mikasorvoja1@gmail.com>
2025-08-27 15:26:20 +0300

    Change about dialog so that it only shows Katja versions for different plan types

--- arho_feature_template/gui/dialogs/plugin_about.py
+++ arho_feature_template/gui/dialogs/plugin_about.py
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 from importlib import resources
 from pathlib import Path
 
+import yaml
 from qgis.PyQt import uic
 from qgis.PyQt.QtWidgets import QDialog, QLabel
 
@@ -8,27 +11,37 @@ ui_path = resources.files(__package__) / "plugin_about.ui"
 FormClass, _ = uic.loadUiType(ui_path)
 
 PLUGIN_PATH = Path(__file__).resolve().parents[2]
-FILE_PATH = PLUGIN_PATH / "metadata.txt"
+FOLDER_PATH = PLUGIN_PATH / "resources" / "libraries" / "regulation_groups"
 
 
 class PluginAbout(QDialog, FormClass):  # type: ignore
     def __init__(self, parent=None):
         super().__init__(parent)
-
         self.setupUi(self)
 
-        self.plugin_version_label: QLabel
-        self.katja_version_label: QLabel
+        self.version_labels: dict[str, QLabel] = {
+            "asemakaava": self.katja_version_town,
+            "yleiskaava": self.katja_version_general,
+            "maakuntakaava": self.katja_version_regional,
+        }
 
         self.show_versions()
 
+    def _get_version_from_file(self, file_path: Path) -> str | None:
+        if not file_path.is_file():
+            return None
+
+        with file_path.open("r") as f:
+            data = yaml.safe_load(f)
+            if "version" in data:
+                return str(data["version"])
+            return None
+
     def show_versions(self):
-        if not FILE_PATH.exists():
+        if not FOLDER_PATH.exists():
             return
 
-        with open(FILE_PATH) as file:
-            for line in file:
-                if line.strip().startswith("version="):
-                    self.plugin_version_label.setText(line.strip().split("=", 1)[1])
-                if line.strip().startswith("katja_version="):
-                    self.katja_version_label.setText(line.strip().split("=", 1)[1])
+        for plan_type, label_widget in self.version_labels.items():
+            file_path = FOLDER_PATH / f"katja_{plan_type}.yaml"
+            version = self._get_version_from_file(file_path)
+            label_widget.setText(version or "-")
