dd832a4b55c3c376cf736d32af78e31ae0b67b75
Miikka Kallio <miikka@gispo.fi>
2026-01-14 07:50:51 +0200

    Allow setting period of validity start date...
    Allow setting period of validity start date when copying plan with lifecycle status "voimassa". WIP

--- arho_feature_template/gui/dialogs/new_plan_dialog.py
+++ arho_feature_template/gui/dialogs/new_plan_dialog.py
@@ -4,7 +4,7 @@ from importlib import resources
 from typing import TYPE_CHECKING
 
 from qgis.PyQt import uic
-from qgis.PyQt.QtCore import Qt, pyqtSignal
+from qgis.PyQt.QtCore import QDate, Qt, pyqtSignal
 from qgis.PyQt.QtWidgets import QComboBox, QDialog, QDialogButtonBox, QLineEdit, QWidget
 
 from arho_feature_template.core.lambda_service import LambdaService
@@ -43,16 +43,15 @@ class NewPlanDialog(QDialog, FormClass):  # type: ignore
         self.plan_name: QLineEdit
         self.plan_lifecycle: CodeComboBox
         self.source_plan: QComboBox
-
         self.button_box: QDialogButtonBox
         self.widget_input: QWidget
         self.widget_progress: QWidget
 
         # INIT
         self.plan = None
-
         self.plan_layer = PlanLayer.get_from_project()
         self.previously_in_edit_mode = self.plan_layer.isEditable()
+
         self.populate_plan_combobox()
         self.plan_lifecycle.populate_from_code_layer(LifeCycleStatusLayer)
         self.plan_name.textChanged.connect(self._check_required_fields)
@@ -62,6 +61,14 @@ class NewPlanDialog(QDialog, FormClass):  # type: ignore
         self.lambda_service = LambdaService()
         self.lambda_service.plan_copied.connect(self._plan_copied)
         self.lambda_service.plan_copy_failed.connect(self._handle_copy_failed)
+
+        # Start date widget initially invisible
+        self.label_validity_start.setVisible(False)
+        self.validity_start_date.setVisible(False)
+
+        # Show/hide start date based on lifecycle stage
+        self.plan_lifecycle.currentTextChanged.connect(self._update_validity_widgets)
+        self._update_validity_widgets(self.plan_lifecycle.currentText())
 
         self._check_required_fields()
 
@@ -86,38 +93,71 @@ class NewPlanDialog(QDialog, FormClass):  # type: ignore
         original_filter = layer.subsetString()
         plans = []
         try:
-            # Make sure plan layer is not in edit state to set new filter
             if self.previously_in_edit_mode:
                 self.plan_layer.rollBack()
-            layer.setSubsetString(f"\"plan_matter_id\"='{active_plan_matter_id}'")  # set temporary filter
+            layer.setSubsetString(f"\"plan_matter_id\"='{active_plan_matter_id}'")
             plans = PlanLayer.models_from_features(list(PlanLayer.get_features()))
         finally:
-            layer.setSubsetString(original_filter)  # restore filter
+            layer.setSubsetString(original_filter)
         return plans
 
     def _check_required_fields(self) -> None:
         ok_button = self.button_box.button(QDialogButtonBox.Ok)
-        if self.plan_name.text() != "" and self.plan_lifecycle.value() is not None:
-            ok_button.setEnabled(True)
-        else:
-            ok_button.setEnabled(False)
+
+        # Required: plan name + lifecycle
+        required_fields_filled = self.plan_name.text() != "" and self.plan_lifecycle.value() is not None
+
+        # If lifecycle stage is "Voimassa", date must also be visible (it will have a date)
+        if self.validity_start_date.isVisible():
+            required_fields_filled = required_fields_filled and self.validity_start_date.date() is not None
+
+        ok_button.setEnabled(required_fields_filled)
+
+    def _update_validity_widgets(self, text: str):
+        """
+        Show the start date widgets only when lifecycle stage is 'Voimassa'.
+        Sets the date to today when visible.
+        """
+        is_voimassa = text == "Voimassa"
+        self.label_validity_start.setVisible(is_voimassa)
+        self.validity_start_date.setVisible(is_voimassa)
+
+        if is_voimassa:
+            # Set the date to current date
+            today_qdate = QDate.currentDate()
+            self.validity_start_date.setDate(today_qdate)
+
+        # Re-check required fields after visibility change
+        self._check_required_fields()
 
     def _on_ok_clicked(self):
         plan_id = self.source_plan.currentData(DATA_ROLE)
+
+        # Only pass the start date if lifecycle stage is "Voimassa"
+        period_of_validity_start = (
+            self.validity_start_date.date().toPyDate() if self.plan_lifecycle.currentText() == "Voimassa" else None
+        )
+
         if plan_id is not None:
             self.widget_input.hide()
             self.widget_progress.show()
             self.adjustSize()
 
-            self.lambda_service.copy_plan(plan_id, self.plan_lifecycle.value(), self.plan_name.text())
+            # Pass the start date only if applicable
+            self.lambda_service.copy_plan(
+                plan_id,
+                self.plan_lifecycle.value(),
+                self.plan_name.text(),
+                period_of_validity_start=period_of_validity_start,
+            )
         else:
             self.plan = Plan(
                 name=self.plan_name.text(),
                 lifecycle_status_id=self.plan_lifecycle.value(),
                 plan_matter_id=get_active_plan_matter_id(),
+                period_of_validity_start=period_of_validity_start,
             )
-            self.plan.geom = PlanLayer.get_active_plan().geom  # Always copy geometry from active plan for now
-            # TODO: Copy details and child ojbect details from selected version
+            self.plan.geom = PlanLayer.get_active_plan().geom
 
             self.accept()
 
