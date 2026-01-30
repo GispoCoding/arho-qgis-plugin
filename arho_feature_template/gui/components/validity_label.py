109673fb6fa120fb21181bf348a6428d36f69df9
Niko Aarnio <niko.aarnio@protonmail.com>
2026-01-30 12:34:49 +0200

    add validity label to plan objects dock, add validity end date to plan object form
    Also made showing validity start/end dates in plan object form consistent with
    plan attribute form (date fields are hidden if not defined).

--- arho_feature_template/gui/components/validity_label.py
+++ arho_feature_template/gui/components/validity_label.py
@@ -1,10 +1,13 @@
-from qgis.PyQt.QtGui import QPixmap
+from qgis.PyQt.QtCore import QSize, Qt
+from qgis.PyQt.QtGui import QIcon, QPixmap, QStandardItem
 from qgis.PyQt.QtWidgets import QLabel, QSizePolicy
 
 from arho_feature_template.core.models import PlanBaseModel
 from arho_feature_template.project.layers.code_layers import LifeCycleStatusLayer
 from arho_feature_template.qgis_plugin_tools.tools.resources import resources_path
 from arho_feature_template.utils.misc_utils import date_as_str
+
+VALIDITY_SORT_ROLE = Qt.UserRole
 
 
 class ValidityLabel(QLabel):
@@ -47,3 +50,46 @@ class ValidityLabel(QLabel):
     def hide_widget(self):
         self.setToolTip("")
         self.hide()
+
+
+def validity_item_from_model(model: PlanBaseModel) -> QStandardItem:
+    """
+    Create a QStandardItem with valid icon, repealed icon or no icon depending on model status.
+
+    Items are meant to be used in PlanObjectDock table.
+
+    Each item has a validity number for sorting stored in `VALIDITY_SORT_ROLE`.
+    0 = valid, 1 = repealed, 0 = undefined.
+    """
+    item = QStandardItem("")
+    item.setEditable(False)
+
+    item.setData(QSize(23, 23), Qt.SizeHintRole)
+
+    if not hasattr(model, "lifecycle_status_id"):
+        item.setData(2, VALIDITY_SORT_ROLE)
+        return item
+
+    if LifeCycleStatusLayer.is_repealed_status(model.lifecycle_status_id):
+        item.setData(QIcon(ValidityLabel.REPEALED_MARK), Qt.DecorationRole)
+
+        if model.period_of_validity_start and model.period_of_validity_end:  # type: ignore
+            tooltip = (
+                "KUMOTTU\n"
+                f"Voimassa {date_as_str(model.period_of_validity_start)} - "  # type: ignore
+                f"{date_as_str(model.period_of_validity_end)}"  # type: ignore
+            )
+        elif model.period_of_validity_end:  # type: ignore
+            tooltip = f"KUMOTTU\nKumottu alkaen: {date_as_str(model.period_of_validity_end)}"  # type: ignore
+        else:
+            tooltip = "KUMOTTU"
+
+        item.setData(1, VALIDITY_SORT_ROLE)
+        item.setData(tooltip, Qt.ToolTipRole)
+
+    elif LifeCycleStatusLayer.is_valid_status(model.lifecycle_status_id):
+        item.setData(QIcon(ValidityLabel.VALID_MARK), Qt.DecorationRole)
+        item.setData(f"VOIMASSA\nVoimassa alkaen: {date_as_str(model.period_of_validity_start)}", Qt.ToolTipRole)  # type: ignore
+        item.setData(0, VALIDITY_SORT_ROLE)
+
+    return item
